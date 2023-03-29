4f4e8075e1fa7cc74eeec292f697783d5c1d26d6
Johannes <hannes@lautr.com>
2023-03-29 15:55:32 +0200

    just a change to see the CI

--- packages/design/components/ui/Button.vue
+++ packages/design/components/ui/Button.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 const newThing = ref(10)
 
-const sayHello = () => {
+const echoOutput = () => {
   newThing.value++
   window.console.log(newThing.value)
 }
@@ -13,8 +13,7 @@ onMounted(() => {
 <template>
   <button
     class="bg-white rounded-sm py-2 px-1 font-bold border-2 border-purple-500 text-purple-700 hover:text-purple-900 hover:border-purple-700 dark:bg-slate-800 dark:text-purple-500 transition-all"
-    @click="sayHello"
-  >
+    @click="echoOutput">
     <!-- @slot button content -->
     <slot />
   </button>
