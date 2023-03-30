a80cd907227644b8e4ea35027a0c63356acf5910
Johannes <hannes@lautr.com>
2023-03-30 09:20:35 +0200

    Linting

--- packages/design/components/ui/Button.vue
+++ packages/design/components/ui/Button.vue
@@ -13,7 +13,8 @@ onMounted(() => {
 <template>
   <button
     class="bg-white rounded-sm py-2 px-1 font-bold border-2 border-purple-500 text-purple-700 hover:text-purple-900 hover:border-purple-700 dark:bg-slate-800 dark:text-purple-500 transition-all"
-    @click="echoOutput">
+    @click="echoOutput"
+  >
     <!-- @slot button content -->
     <slot />
   </button>
