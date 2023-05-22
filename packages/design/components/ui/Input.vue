9e1ad051ea377991a9abf4b570e9b6e621e2fd16
Johannes Lauter <hannes@lautr.com>
2023-05-22 10:19:10 +0200

    Update Input.vue

--- packages/design/components/ui/Input.vue
+++ packages/design/components/ui/Input.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-const { input } = defineModels<{
-  input?: string
+const { modelValue } = defineModels<{
+  modelValue?: string
 }>()
 </script>
 
 <template>
   <input
-    v-model="input"
+    v-model="modelValue"
     class="block w-full rounded h-10 px-3 border border-stone-300 bg-white outline-none focus:border-stone-900 dark:bg-stone-900 dark:border-stone-700 dark:focus:border-stone-500"
   >
 </template>
