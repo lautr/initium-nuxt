9ee48f07342be3d721aa1d5a9598aceb6d9f9702
Johannes Lauter <johannes.lauter@checkout-charlie.com>
2024-07-10 13:55:45 +0200

    debug

--- apps/webapp/nuxt.config.ts
+++ apps/webapp/nuxt.config.ts
@@ -45,7 +45,8 @@ export default defineNuxtConfig({
   tailwindcss: {
     config: {
       content: [
-        './node_modules/@lautr/initium-nuxt-design/**/*.vue',
+        '../../packages/design/components/**/*.vue',
+        '../../packages/design/styles/*.css',
         './components/**/*.{js,vue,ts}',
         './layouts/**/*.vue',
         './pages/**/*.vue',
@@ -65,8 +66,59 @@ export default defineNuxtConfig({
     shim: false,
   },
   vite: {
-    resolve: {
-      preserveSymlinks: true,
+    build: {
+      rollupOptions: {
+        output: {
+          chunkFileNames: '_nuxt/cc-[hash].js',
+          assetFileNames: '_nuxt/cc-[hash][extname]',
+          entryFileNames: '_nuxt/cc-[hash].js',
+          // target ~250KB per chunk in an ideal world
+          experimentalMinChunkSize: 250 * 1024,
+          manualChunks: (id: string) => {
+            // need to avoid touching non-entrypoint files, otherwise it breaks bundling
+            // because imports aren't idempotent
+            if (
+              !id.includes('node_modules')
+              && !id.startsWith('virtual:')
+              && !id.includes('src')
+              && !id.includes('assets')
+            ) {
+              // merge pages/foo/* as chunk-pg-foo, pages/bar/* as chunk-pg-bar, etc.
+              // then merge pages/* (ie no subfolder) into chunk-pg-misc
+              if (id.includes('pages')) {
+                const parts = id.split('/')
+                const folderIndex = parts.indexOf('pages')
+                if (folderIndex + 2 < parts.length) {
+                  const pageGroup = parts[folderIndex + 1]
+                  return `chunk-pg-${pageGroup}`
+                }
+                return 'chunk-pg-misc'
+              }
+            }
+          },
+        },
+      },
+    },
+  },
+  hooks: {
+    'build:manifest': (manifest) => {
+      // removes css files from output to avoid blocking requests
+      // this is a workaround for https://github.com/nuxt/nuxt/issues/21821
+      Object.keys(manifest).forEach((key: string) => {
+        if (manifest[key].css)
+          manifest[key].css = []
+      })
+
+      for (const key in manifest) {
+        manifest[key].dynamicImports = []
+
+        const file = manifest[key]
+        if (file.assets) {
+          file.assets = file.assets.filter(
+            assetName => !/.+\.gif|jpe?g|png|svg$/.test(assetName),
+          )
+        }
+      }
     },
   },
 })
